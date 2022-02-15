5274f39d6dc7b6709d5a415c5b999c8bbe7ff750
Panos Triantafyllou <panos30fyllou@gmail.com>
2022-02-16 01:28:55 +0200

    Second Test

--- todos-api/spec/rails_helper.rb
+++ todos-api/spec/rails_helper.rb
@@ -96,3 +96,13 @@ RSpec.configure do |config|
   # [...]
 end
 
+# [...]
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# [...]
+RSpec.configure do |config|
+  # [...]
+  config.include RequestSpecHelper, type: :request
+  # [...]
+end
+
+
